9de501d0eb3f672b99bf3f4e0ad36b7af92c4522
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-10-14 13:28:33 -0500

    comer client validation added

--- dist/modules/sera/comer-client/dto/comer-client.dto..js
+++ dist/modules/sera/comer-client/dto/comer-client.dto..js
@@ -10,12 +10,261 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ComerClientDto = void 0;
-const swagger_1 = require("@nestjs/swagger");
+const class_validator_1 = require("class-validator");
 class ComerClientDto {
 }
 __decorate([
-    (0, swagger_1.ApiProperty)({ example: "No. Cliente" }),
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsNotEmpty)(),
     __metadata("design:type", Number)
 ], ComerClientDto.prototype, "clientId", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(100),
+    (0, class_validator_1.IsNotEmpty)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "reasonName", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(20),
+    (0, class_validator_1.IsNotEmpty)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "rfc", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "sellerId", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(8),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "street", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "city", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "colony", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(49),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "delegacion", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(6),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "cp", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(22),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "country", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(20),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "fax", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "phone", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "mailWeb", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(31),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "state", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(20),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "curp", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(2),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "blackList", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "paternalSurname", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(60),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "maternalSurname", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "muniId", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "estaId", void 0);
+__decorate([
+    (0, class_validator_1.IsDateString)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Date)
+], ComerClientDto.prototype, "blackListDate", void 0);
+__decorate([
+    (0, class_validator_1.IsDateString)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Date)
+], ComerClientDto.prototype, "releaseDate", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "penaltyId", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(1),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "personType", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(15),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "approvedRfc", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(30),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "userFree", void 0);
+__decorate([
+    (0, class_validator_1.IsDateString)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Date)
+], ComerClientDto.prototype, "freeDate", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "registerId", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(20),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "economicActCve", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "identificationType", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(50),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "identificationNumber", void 0);
+__decorate([
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.IsPositive)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Number)
+], ComerClientDto.prototype, "agentId", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(40),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "outsideNo", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(40),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "insisdeNo", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(50),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "password", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(50),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "user", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(18),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "interbankKey", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(3),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "bank", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(10),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "branch", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(11),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "checksAccount", void 0);
+__decorate([
+    (0, class_validator_1.IsDateString)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Date)
+], ComerClientDto.prototype, "penaltyInDate", void 0);
+__decorate([
+    (0, class_validator_1.IsDateString)(),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", Date)
+], ComerClientDto.prototype, "penaltyOutDate", void 0);
+__decorate([
+    (0, class_validator_1.IsString)(),
+    (0, class_validator_1.MaxLength)(30),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ComerClientDto.prototype, "penalizeUser", void 0);
 exports.ComerClientDto = ComerClientDto;
 //# sourceMappingURL=comer-client.dto..js.map
--- src/modules/sera/comer-client/dto/comer-client.dto..ts
+++ src/modules/sera/comer-client/dto/comer-client.dto..ts
@@ -1,90 +1,221 @@
-import { ApiProperty } from "@nestjs/swagger";
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from "class-validator";
 
 export class ComerClientDto {
-  @ApiProperty({ example: "No. Cliente" })
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   clientId: number;
 
+  @IsString()
+  @MaxLength(100)
+  @IsNotEmpty()
   reasonName: string;
 
+  @IsString()
+  @MaxLength(20)
+  @IsNotEmpty()
   rfc: string;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   sellerId: number | null;
 
+  @IsString()
+  @MaxLength(8)
+  @IsOptional()
   street: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   city: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   colony: string | null;
 
+  @IsString()
+  @MaxLength(49)
+  @IsOptional()
   delegacion: string | null;
 
+  @IsString()
+  @MaxLength( 6)
+  @IsOptional()
   cp: string | null;
 
+  @IsString()
+  @MaxLength(22)
+  @IsOptional()
   country: string | null;
 
+  @IsString()
+  @MaxLength(20)
+  @IsOptional()
   fax: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   phone: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   mailWeb: string | null;
 
+  @IsString()
+  @MaxLength(31)
+  @IsOptional()
   state: string | null;
 
+  @IsString()
+  @MaxLength(20)
+  @IsOptional()
   curp: string | null;
 
+  @IsString()
+  @MaxLength(2)
+  @IsOptional()
   blackList: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   paternalSurname: string | null;
 
+  @IsString()
+  @MaxLength(60)
+  @IsOptional()
   maternalSurname: string | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   muniId: number | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   estaId: number | null;
 
+  @IsDateString()
+  @IsOptional()
   blackListDate: Date | null;
 
+  @IsDateString()
+  @IsOptional()
   releaseDate: Date | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   penaltyId: number | null;
 
+  @IsString()
+  @MaxLength(1)
+  @IsOptional()
   personType: string | null;
 
+  @IsString()
+  @MaxLength(15)
+  @IsOptional()
   approvedRfc: string | null;
 
+  @IsString()
+  @MaxLength(30)
+  @IsOptional()
   userFree: string | null;
 
+  @IsDateString()
+  @IsOptional()
   freeDate: Date | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   registerId: number | null;
 
+  @IsString()
+  @MaxLength(20)
+  @IsOptional()
   economicActCve: string | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   identificationType: number | null;
 
+  @IsString()
+  @MaxLength(50)
+  @IsOptional()
   identificationNumber: string | null;
 
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   agentId: number | null;
 
+  @IsString()
+  @MaxLength(40)
+  @IsOptional()
   outsideNo: string | null;
 
+  @IsString()
+  @MaxLength(40)
+  @IsOptional()
   insisdeNo: string | null;
 
+  @IsString()
+  @MaxLength(50)
+  @IsOptional()
   password: string | null;
 
+  @IsString()
+  @MaxLength(50)
+  @IsOptional()
   user: string | null;
 
+  @IsString()
+  @MaxLength(18)
+  @IsOptional()
   interbankKey: string | null;
 
+  @IsString()
+  @MaxLength(3)
+  @IsOptional()
   bank: string | null;
 
+  @IsString()
+  @MaxLength(10)
+  @IsOptional()
   branch: string | null;
 
+  @IsString()
+  @MaxLength(11)
+  @IsOptional()
   checksAccount: string | null;
 
+  @IsDateString()
+  @IsOptional()
   penaltyInDate: Date | null;
 
+  @IsDateString()
+  @IsOptional()
   penaltyOutDate: Date | null;
 
+  @IsString()
+  @MaxLength(30)
+  @IsOptional()
   penalizeUser: string | null;
 }
