3ff09008abe39b517df30a7f1620aac4e63136f4
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-11-22 07:23:59 -0600

    comer-agreeement dto to update

--- src/modules/sera/comer-agreement-events/dto/comer-agreement-events.dto.ts
+++ src/modules/sera/comer-agreement-events/dto/comer-agreement-events.dto.ts
@@ -1,6 +1,5 @@
 import { Transform, Type } from "class-transformer";
 import {
-  IsDateString,
   IsNotEmpty,
   IsNumber,
   IsOptional,
--- src/modules/sera/comer-agreement-events/dto/update-comer-agreement-events.dto.ts
+++ src/modules/sera/comer-agreement-events/dto/update-comer-agreement-events.dto.ts
@@ -1,9 +1,39 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform, Type } from "class-transformer";
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+} from "class-validator";
 
+import { Message } from "src/shared/validation-messages/message";
 export class UpdateComerConvEventDto {
-  
+  @Type(() => Number)
+  @IsNotEmpty({
+    message: Message.REQUIRED("$property"),
+  })
+  @IsNumber(
+    {},
+    {
+      message: Message.NUMBER("$property"),
+    }
+  )
+  @IsPositive()
+  @ApiProperty({ example: 1})
   eventIdToUpdt: number;
 
+  @Type(() => Number)
+  @IsNotEmpty({
+    message: Message.REQUIRED("$property"),
+  })
+  @IsNumber(
+    {},
+    {
+      message: Message.NUMBER("$property"),
+    }
+  )
+  @IsPositive()
+  @ApiProperty({ example: 1})
   announcementEventIdToUpdt: number;
   
 }
